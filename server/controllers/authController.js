3c75504138b1490e6f2cf7642a80c4e581cf6329
Vinzzztty <kevinarnandes21@gmail.com>
2024-01-07 23:32:24 +0700

    add: complete simple login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -43,3 +43,31 @@ exports.signup = async (req, res) => {
         });
     }
 };
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(401).json({
+                message: "Email not found",
+            });
+        }
+
+        if (user.password !== password) {
+            return res.status(401).json({
+                message: "Invalid password",
+            });
+        }
+
+        res.status(200).json({
+            message: "Login Successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "An unexpected error occured",
+            error: error.message,
+        });
+    }
+};
